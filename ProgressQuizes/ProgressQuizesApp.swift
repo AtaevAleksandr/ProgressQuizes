ce641cf575e655a8f19af5beb9e17560cde151d4
Aleksandr Ataev <onlyaa94@mail.ru>
2024-05-29 14:54:04 +0300

    Initial Commit

---
+++ ProgressQuizes/ProgressQuizesApp.swift
@@ -0,0 +1,17 @@
+//
+//  ProgressQuizesApp.swift
+//  ProgressQuizes
+//
+//  Created by Aleksandr Ataev on 29.05.2024.
+//
+
+import SwiftUI
+
+@main
+struct ProgressQuizesApp: App {
+    var body: some Scene {
+        WindowGroup {
+            ContentView()
+        }
+    }
+}
